31037b3a25bba106ec178cc16eb329860ad23cde
heyitskaili <63416127+heyitskaili@users.noreply.github.com>
2020-11-08 22:37:11 -0500

    added files

---
+++ dev/js/scripts.js
@@ -0,0 +1,14 @@
+// import {gsap} from "gsap";
+// import {PattlesAnimation} from "./sections/pattles.js"
+// import {FaceAnimation} from "./sections/face.js"
+// import {PotAnimation} from "./sections/pot.js"
+// import {LineAnimation} from "./sections/greenline.js"
+
+
+// const mainTL = gsap.timeline();
+// mainTL.add(PotAnimation());
+// mainTL.add(LineAnimation());
+// mainTL.add(FaceAnimation());
+// mainTL.add(PattlesAnimation());
+
+
---
+++ dev/js/sections/pot.js
@@ -0,0 +1,10 @@
+// import {gsap} from "gsap";
+
+// gsap.set("#pot",{transformOrigin: "center"});
+
+// export function PotAnimation(){
+//     var tl = gsap.timeline();
+//     tl.from("#pot",{duration:0.7, scale:50});
+
+//     return tl;
+// }
