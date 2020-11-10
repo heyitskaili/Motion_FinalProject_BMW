9054f42cb3796063f820b9670a3af054dc16b6ce
heyitskaili <63416127+heyitskaili@users.noreply.github.com>
2020-11-10 18:45:44 -0500

    added fingerprintsection

--- dev/js/sections/fingerprintsection/fingerprint.js
+++ dev/js/sections/fingerprintsection/fingerprint.js
@@ -6,6 +6,18 @@ gsap.registerPlugin(DrawSVGPlugin);
 export function FingerprintAnimation() {
     var fingerprinttl = gsap.timeline();
 
+    fingerprinttl.from("#logo", {
+        duration:0.3,
+        alpha: -1
+        
+    });
+
+    fingerprinttl.from("#outerline, #triangleright, #triangleleft", {
+        duration: 1,
+        drawSVG: "83% 83%"
+        
+    });
+
 
     return fingerprinttl;
 }
