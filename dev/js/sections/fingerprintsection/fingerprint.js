bd2687505ea43e37705c8aa86952d1a39ae58578
heyitskaili <63416127+heyitskaili@users.noreply.github.com>
2020-11-10 21:33:02 -0500

    started fingerprint section

--- dev/js/sections/fingerprintsection/fingerprint.js
+++ dev/js/sections/fingerprintsection/fingerprint.js
@@ -1,22 +1,55 @@
-import { gsap } from "gsap";
-import { DrawSVGPlugin} from "gsap/DrawSVGPlugin";
-
+import {gsap} from "gsap";
+import {DrawSVGPlugin} from "gsap/DrawSVGPlugin";
 gsap.registerPlugin(DrawSVGPlugin);
+
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+gsap.registerPlugin(ScrambleTextPlugin);
+
 
 export function FingerprintAnimation() {
     var fingerprinttl = gsap.timeline();
 
     fingerprinttl.from("#logo", {
-        duration:0.3,
+        duration: 0.3,
         alpha: -1
-        
+
     });
 
     fingerprinttl.from("#outerline, #triangleright, #triangleleft", {
         duration: 1,
         drawSVG: "83% 83%"
-        
+
     });
+
+    fingerprinttl.from("#line1, #line2, #line3, #line4, #line5, #line6", {
+        duration: 1,
+        drawSVG: "50% 50%",
+        alpha: -1
+
+    });
+
+    fingerprinttl.from("#outersquare", {
+        duration: 0.3,
+        alpha: -1
+    });
+
+    fingerprinttl.from("#fingerprintscanning", {
+
+        duration: 0.3,
+        alpha: -1
+
+    });
+
+    // fingerprinttl.to("#fingerprintscanning", {
+        
+    //     scrambleText: {
+    //         duration: 3,
+    //         text: "ScrambleText allows you to animate the scrambling of text.",
+    //         chars: "lowerCase",
+    //         revealDelay: 0.5,
+    //         tweenLength: true
+    //     }
+    // })
 
 
     return fingerprinttl;
