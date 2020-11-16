f7e84bc920a30153e4a475966a97899c65755a28
heyitskaili <63416127+heyitskaili@users.noreply.github.com>
2020-11-15 23:54:40 -0500

    deleted unneeded stuff

--- dev/js/sections/maintabletsection/maintablet.js
+++ dev/js/sections/maintabletsection/maintablet.js
@@ -12,32 +12,44 @@ export function MaintabletAnimation() {
     var maintablettl = gsap.timeline();
 
     maintablettl.from("#innerline, #triangleright2outline, #triangleright3outline, #triangleright4outline, #triangleright5outline, #triangleright6outline, #triangleright7outline, #triangleright8outline, #triangleright9outline,#triangleleft9outline,#triangleleft8outline,#triangleleft7outline, #triangleleft6outline, #triangleleft5outline, #triangleleft4outline, #triangleleft3outline, #triangeleft2outline", {
-        duration: 2,
+        duration: 1,
         drawSVG: "50% 50%"
 
     });
 
-    maintablettl.from("#lightdetail,#triangleright2, #triangleright9, #triangeleft2, #triangleleft9", {
-        duration: 0.4,
+    maintablettl.from("#lightdetail,#triangleright2, #triangleright9, #triangeleft2, #triangleleft9, #milesection2", {
+        duration: 0.3,
         alpha: -1
 
     });
 
-    maintablettl.from("#light, #triangleleft8,#triangleleft7,#triangleright8,#triangleright5", {
-        duration: 0.4,
+    maintablettl.from("#light, #triangleleft8,#triangleleft7,#triangleright8,#triangleright5, #milesection3, #milesection4", {
+        duration: 0.3,
         alpha: -1
 
     });
 
-    maintablettl.from("#cardetail, #triangleleft5, #triangleleft6, #triangleright4, #triangleright7", {
-        duration: 0.4,
+    maintablettl.from("#cardetail, #triangleleft5, #triangleleft6, #triangleright4, #triangleright7, #milesection1, #milesection5", {
+        duration: 0.3,
         alpha: -1
 
     });
 
-    maintablettl.from("#carinside, #triangleleft3, #triangleleft4, #triangleright3, #triangleright6", {
-        duration: 0.4,
+    maintablettl.from("#carinside, #triangleleft3, #triangleleft4, #triangleright3, #triangleright6,#milesection6, #milesection7", {
+        duration: 0.3,
         alpha: -1
+
+    });
+
+    maintablettl.from("#miles,#milenumber, #VoiceControlonpath, #VoiceControlon", {
+        duration: 0.3,
+        alpha: -1
+
+    });
+
+    maintablettl.from("#Voice", {
+        duration: 0.8,
+        drawSVG: "50% 50%"
 
     });
 
